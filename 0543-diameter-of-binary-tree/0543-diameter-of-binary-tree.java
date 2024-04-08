dcebc2e1ef526fb33479efa645d18e7b51a439cc
Debapriya Das <debapriyo00@gmail.com>
2024-04-08 08:35:54 +0530

    Time: 0 ms (100.00%), Space: 44.5 MB (18.63%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -14,60 +14,20 @@
  * }
  */
 class Solution {
-    //crete a class ..
-    static class Info{
-        int dia;
-        int ht;
-
-        //constructor..
-        public Info(int dia , int ht){
-            this.ht = ht;
-            this.dia = dia;
-        }
-    }
-    
+    int diameter = 0;
     public int diameterOfBinaryTree(TreeNode root) {
-        /*base case.
-         if(root == null){
-            return 0;
-        }
-        int leftDia = diameterOfBinaryTree(root.left);
-        int leftHt = heightofNode(root.left);
-        int rightDia = diameterOfBinaryTree(root.right);
-        int rightHt = heightofNode(root.right);
-        
-        int selfDia = leftHt + rightHt;
-        return Math.max(selfDia, Math.max(rightDia, leftDia));
-
-    }
-
-      // calculate the heigth of the tree..
-       public static int heightofNode(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        int lh = heightofNode(root.left);
-        int rh = heightofNode(root.right);
-        return Math.max(lh, rh) + 1;*/
-        
-        
-        return --diameter(root).dia;
+        height(root);
+        return --diameter; //this (diameter -1) another syntax
     }
     
-    //create a methord to claculate the diameter.
-    public static Info diameter(TreeNode root){
-        //base case..
-        if(root == null){
-            return new Info (0,0);
-        }
-        //calcuate left and rigth info.
-        Info Leftinfo = diameter(root.left);
-        Info Rightinfo = diameter(root.right);
-
-        int dia = Math.max(Math.max(Leftinfo.dia ,Rightinfo.dia) ,Leftinfo.ht + Rightinfo.ht+1);
-        int ht = Math.max(Leftinfo.ht,Rightinfo.ht)+1;
-        return new Info(dia, ht);
+    int height(TreeNode node){
+        if(node == null)return 0;
+        int leftHeight = height(node.left);
+        int rightHeight = height(node.right);
+        
+        int Selfdia = leftHeight + rightHeight+1;
+        diameter = Math.max(diameter,Selfdia);
+        
+        return Math.max(leftHeight, rightHeight) +1;
     }
-    
-    
 }
